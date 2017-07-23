f598480bff43da0871588c07b961e197fde5f6df
xel <mcfrankline@gmail.com>
2017-07-23 18:30:00 +0000

    create Today sectiongit

--- coinprice/src/App.js
+++ coinprice/src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import React and Component
+    import React, { Component } from 'react';
+    // Import CSS from App.css
+    import './App.css';
+    // Import the Today component to be used below
+    import Today from './Today/Today'
+    // Import the History component to be used below
+    import History from './History/History'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+    class App extends Component {
+      render() {
+        return (
+          <div className="">
+              <div className="topheader">
+                  <header className="container">
+                      <nav className="navbar">
+                          <div className="navbar-brand">
+                              <span className="navbar-item">Coinprice</span>
+                          </div>
+                          <div className="navbar-end">
+                              // <a className="navbar-item" href="https://pusher.com" target="_blank" rel="noopener noreferrer">Co</a>
+                          </div>
+                      </nav>
+                  </header>
+              </div>
+              <section className="results--section">
+                  <div className="container">
+                      <h1>Coinprice is a realtime price information about<br></br> Bitcoin, Ethereum and Litecoin .</h1>
+                  </div>
+                  <div className="results--section__inner">
+                      <Today />
+                      <History />
+                  </div>
+              </section>
+          </div>
+        );
+      }
+    }
 
-export default App;
+    export default App;
